08961997173ddcf35da412aae847e4fd15b32b68
Olaf Alders <olaf@wundersolutions.com>
2016-06-08 22:10:51 -0400

    Response content could be a HashRef or an ArrayRef.

--- lib/WebService/HealthGraph/Response.pm
+++ lib/WebService/HealthGraph/Response.pm
@@ -3,11 +3,14 @@ package WebService::HealthGraph::Response;
 use Moo;
 
 use JSON::MaybeXS qw( decode_json );
-use Types::Standard qw( Bool HashRef InstanceOf Maybe );
+use Types::Standard qw( Bool InstanceOf Maybe Ref );
+
+# records could be an ArrayRef. other than that, we should be mostly dealing
+# with a HashRef
 
 has content => (
     is      => 'ro',
-    isa     => Maybe [HashRef],
+    isa     => Maybe [Ref],
     lazy    => 1,
     builder => '_build_content',
 );
