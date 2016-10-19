0e3fe73540336fd7f19bccff65a71c9715195d50
Olaf Alders <olaf@wundersolutions.com>
2016-10-18 20:26:17 -0400

    Adds a script for weight feed to examples.

---
+++ examples/weight.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Data::Printer;
+use DateTime ();
+use URI::FromHash qw( uri );
+use WebService::HealthGraph ();
+
+my $runkeeper = WebService::HealthGraph->new(
+    debug => 1,
+    token => $ENV{HEALTHGRAPH_TOKEN},
+);
+
+my $user = $runkeeper->user;
+
+p $user->content;
+
+# Fetch a weight feed
+my $cutoff = DateTime->now->subtract( days => 28 );
+
+my $uri = uri(
+    path            => '/weight',
+    query           => { noEarlierThan => $cutoff->ymd, pageSize => 10, },
+    query_separator => '&',
+);
+
+my $feed = $runkeeper->get( $uri, { feed => 1 } );
+
+my $count;
+while ( my $item = $feed->next ) {
+    ++$count;
+    last if $count == 20;
+}
